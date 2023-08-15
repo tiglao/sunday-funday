41eba6d22112b58a77c14b077a7fbda24cf7b08c
John <johnbjr76@gmail.com>
2023-08-15 08:43:34 -0600

    initial setup of mongodb and test CRUD endpoints to model from

--- api/main.py
+++ api/main.py
@@ -1,28 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from routes import test
 
 app = FastAPI()
+app.include_router(test.router, tags=["tests"], prefix="/test")
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/api/launch-details")
-def launch_details():
-    return {
-        "launch_details": {
-            "module": 3,
-            "week": 17,
-            "day": 5,
-            "hour": 19,
-            "min": "00"
-        }
-    }
