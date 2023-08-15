50246f8478d40323d61f971f25756458998289aa
Ellie <26285228+tiglao@users.noreply.github.com>
2023-08-15 14:17:28 -0700

    Add Invitation models.

--- api/queries/invitations.py
+++ api/queries/invitations.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import BaseModel
 from typing import List, Optional
 
 
 class Invitation(BaseModel):
+    account_id: str
+    rsvpStatus: str
 
 
 class InvitationList(BaseModel):
