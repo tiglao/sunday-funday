06cef7f04ce1781bc8d5eb015ca8af9433b3786e
John <johnbjr76@gmail.com>
2023-08-16 16:39:24 -0600

    install jwtdown-for-react

---
+++ ghi/src/UserDashboard.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function UserDashboard() {
+  return (
+    <div>
+      <h1>UserDashboard</h1>
+    </div>
+  );
+}
+
+export default UserDashboard;
