48a7ded26d40b8b489766876c84a9040a0c21d72
Ellie <26285228+tiglao@users.noreply.github.com>
2023-08-23 12:34:12 -0700

    Add most recent changes to main. API endpoints unverified.

--- api/models/accounts.py
+++ api/models/accounts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pydantic import BaseModel, EmailStr
 from jwtdown_fastapi.authentication import Token
 from typing import Optional, List, Dict, Union
@@ -42,41 +41,6 @@ class AccountCreate(BaseModel):
 
 class AccountUpdate(BaseModel):
     pass
-=======
-from pydantic import BaseModel
-from jwtdown_fastapi.authentication import Token
-
-
-class LogOut(BaseModel):
-    account_id: str
-
-
-class LogIn(BaseModel):
-    email: str
-    password: str
-    first_name: str
-    last_name: str
-
-
-class Account(BaseModel):
-    email: str
-    password: str
-    first_name: str
-    last_name: str
-    date_of_birth: str
-    avatar: str
-    user_name: str
-    id: str
-
-
-class AccountUpdate(BaseModel):
-    email: str
-    password: str
-    first_name: str
-    last_name: str
-    date_of_birth: str
-    avatar: str
->>>>>>> 86fb5d18c9f9c3be517833d054c07294ef3801fe
 
 
 class DuplicateAccountError(ValueError):
@@ -84,10 +48,7 @@ class DuplicateAccountError(ValueError):
 
 
 class AccountIn(BaseModel):
-<<<<<<< HEAD
     email: EmailStr
-=======
->>>>>>> 86fb5d18c9f9c3be517833d054c07294ef3801fe
     username: str
     password: str
     full_name: str
@@ -95,11 +56,7 @@ class AccountIn(BaseModel):
 
 class AccountOut(BaseModel):
     id: str
-<<<<<<< HEAD
     email: str
-=======
-    username: str
->>>>>>> 86fb5d18c9f9c3be517833d054c07294ef3801fe
     full_name: str
 
 
@@ -108,18 +65,11 @@ class AccountOutWithPassword(AccountOut):
 
 
 class AccountForm(BaseModel):
-<<<<<<< HEAD
     email: str
     password: str
 
 
 # token parameter, creates token
-=======
-    username: str
-    password: str
-
-
->>>>>>> 86fb5d18c9f9c3be517833d054c07294ef3801fe
 class AccountToken(Token):
     account: AccountOut
     # pass
