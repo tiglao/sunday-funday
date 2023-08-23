7f6564b370ca8ad102dfbfc8a9358cad96a11b8e
Ellie Tiglao <hello@ellietiglao.com>
2023-08-23 21:31:10 +0000

    Revert "Add most recent model changes to main."
    This reverts commit 2bf76e7d475f39ca59905a581a78b44abc6d4afd

--- api/models/locations.py
+++ api/models/locations.py
@@ -1,16 +1,21 @@
 from pydantic import BaseModel, Field, HttpUrl
-from typing import Optional, List, Dict, Union
+from typing import Optional, List, Dict
 from uuid import UUID
 
 
-# people can favorite locations, so what should happen is that each location has a set of account ids that have favorited this location along with the location_tags they've chosen for the specific location.
 class Location(BaseModel):
     id: UUID
     place_id: str
     note_ids: Optional[List[str]]
     favorite_status: bool = False
-    party_plans: Optional[List[str]]
-    user_tags: Optional[List[Dict[str, Union[str, List[str]]]]]
+    account_ids: List[str] = []
+    account_location_tags: Optional[Dict[str, List[str]]]
+    # note_ids: Optional[List[UUID]]
+    # favorite_status: bool = False
+    # account_ids: List[UUID]
+    # account_location_tags: Optional[Dict[UUID, List[str]]]
+    # account_ids: List[UUID]
+    # account_location_tags: Optional[Dict[UUID, List[str]]]
 
     class Config:
         allow_population_by_field_name = True
@@ -22,16 +27,12 @@ class Location(BaseModel):
                     "123e4567-e89b-12d3-a456-426614174001",
                     "123e4567-e89b-12d3-a456-426614174002",
                 ],
-                "user_tags": [
-                    {
-                        "account_id": "123e4567-e89b-12d3-a456-426614174001",
-                        "tags": ["tag1", "tag2"],
-                    },
-                    {
-                        "account_id": "123e4567-e89b-12d3-a456-426614174002",
-                        "tags": ["tag3"],
-                    },
-                ],
+                "favorite_status": True,
+                "account_ids": ["123e4567-e89b-12d3-a456-426614174001"],
+                "account_location_tags": {
+                    "123e4567-e89b-12d3-a456-426614174001": {"tag1", "tag2"},
+                    "123e4567-e89b-12d3-a456-426614174002": {"tag3"},
+                },
             }
         }
 
@@ -39,37 +40,54 @@ class Location(BaseModel):
 class LocationCreate(BaseModel):
     place_id: str
     favorite_status: bool = False
+    account_id: str
+    account_location_tags: Optional[Dict[str, List[str]]]
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
                 "place_id": "76565765",
-                "favorite_status": False,
+                "favorite_status": True,
+                "account_id": "123e4567-e89b-12d3-a456-426614174001",  # example UUID
+                "account_location_tags": {
+                    "123e4567-e89b-12d3-a456-426614174001": ["tag1", "tag2"],
+                },
             }
         }
 
 
 class LocationUpdate(BaseModel):
     note_ids: Optional[List[str]]
-    favorite_status: Optional[bool]
-    party_plans: Optional[List[str]]
-    user_tags: Optional[List[Dict[str, Union[str, List[str]]]]]
+    favorite_status: bool = False
+    account_ids: List[str] = []
+    account_location_tags: Optional[Dict[str, List[str]]]
+    # note_ids: Optional[List[UUID]]
+    # favorite_status: bool = False
+    # account_ids: List[UUID]
+    # account_location_tags: Optional[Dict[UUID, List[str]]]
+    # account_ids: List[UUID]
+    # account_location_tags: Optional[Dict[UUID, List[str]]]
 
     class Config:
         schema_extra = {
             "example": {
-                "note_ids": [
-                    "123e4567-e89b-12d3-a456-426614174001",
-                    "123e4567-e89b-12d3-a456-426614174002",
-                ],
-                "favorite_status": False,
-                "party_plans": ["123e4567-e89b-12d3-a456-426614174001"],
-                "user_tags": [
-                    {
-                        "account_id": "123e4567-e89b-12d3-a456-426614174001",
-                        "tags": ["tag1", "tag2"],
-                    },
-                ],
+                "general_location": "Denver",
+                "party_date": "2022-02-23",
+                "start_time": "2022-02-23T14:30:00",
+                "end_time": "2022-02-23T17:30:00",
+                "description": "Updated Description here ....",
+                "image": "https://picsum.photos/201",
+                "party_status": "share draft",
+                "invitations": {"name3": "id3", "name4": "id4"},
+                "keywords": ["party", "drinks", "dance"],
+                "favorite_locations": {
+                    "new_location1": "new_place_id1",
+                    "new_location2": "new_place_id2",
+                },
+                "chosen_locations": {
+                    "new_locationX": "new_place_idX",
+                    "new_locationY": "new_place_idY",
+                },
             }
         }
