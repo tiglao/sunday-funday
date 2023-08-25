110c37f6d8ae545e573e65dd72255b4c19341f22
Ellie Tiglao <hello@ellietiglao.com>
2023-08-25 18:24:58 +0000

    Google Maps API Location

--- api/models/party_plans.py
+++ api/models/party_plans.py
@@ -1,94 +1,148 @@
 from pydantic import BaseModel, Field, HttpUrl
 from typing import Optional, Dict, List
 from datetime import date, datetime
-import uuid
+from uuid import UUID
+from enum import Enum
+
+
+class PartyStatus(str, Enum):
+    DRAFT = "draft"
+    SHARE_DRAFT = "share draft"
+    FINALIZED = "finalized"
+
+
+class GeoJSON(BaseModel):
+    type: str = "Point"
+    coordinates: List[float] = Field(..., min_items=2, max_items=2)
+    expires: Optional[datetime]
+
+
+class ApiMapsLocation(BaseModel):
+    geo: Optional[GeoJSON]
+    input: Optional[str]
 
 
 class PartyPlan(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    username: str
-    notes: str
-    date: date
-    invitations: Optional[Dict[str, str]]
-    start_time: datetime
-    end_time: datetime
-    party_status: str
-    keywords: List[str]
-    general_location: str
-    favorite_locations: Optional[Dict[str, str]]
-    description: str
+    id: UUID
+    account_id: str
+    created: datetime
+    updated: Optional[datetime]
+    api_maps_location: List[ApiMapsLocation]
+    start_time: Optional[datetime]
+    end_time: Optional[datetime]
+    description: Optional[str]
     image: Optional[HttpUrl]
+    party_status: Optional[PartyStatus]
+    invitations: Optional[List[UUID]]
+    keywords: Optional[List[str]]
+    searched_locations: Optional[List[UUID]]
+    favorite_locations: Optional[List[UUID]]
+    chosen_locations: Optional[List[UUID]]
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "username": "Billy1234",
-                "notes": "Notes go here....",
-                "date": "2022-02-22",
-                "invitees": {
-                    "name": "id",
-                    "name": "id",
-                    "name": "id",
-                    "name": "id",
-                },
-                "start_time": "2022-02-22 14:30",
-                "end_time": "2022-02-22 17:30",
-                "party_status": "draft",
-                "keywords": ["fun", "bar", "burgers"],
-                "general_location": "Denver",
-                "favorite_locations": {
-                    "location name": "place_id",
-                    "location name": "place_id",
-                    "location name": "place_id",
-                    "location name": "place_id",
-                },
+                "id": "123e4567-e89b-12d3-a456-426614174000",
+                "account_id": "123456789",
+                "created": "2022-02-22T14:30:00",
+                "updated": "2022-02-22T16:30:00",
+                "api_maps_location": [
+                    {
+                        "geo": {
+                            "type": "Point",
+                            "coordinates": [40.7128, -74.0060],
+                            "expires": "2023-09-24T22:42:00.402000",
+                        },
+                        "input": "New York, NY",
+                    }
+                ],
+                "start_time": "2022-02-22T14:30:00",
+                "end_time": "2022-02-22T17:30:00",
                 "description": "Description here ....",
                 "image": "https://picsum.photos/200",
+                "party_status": "draft",
+                "invitations": ["id1", "id2"],
+                "keywords": ["fun", "bar", "burgers"],
+                "searched_locations": [
+                    "location_id1",
+                    "location_id2",
+                    "location_id3",
+                ],
+                "favorite_locations": ["location_id1", "location_id2"],
+                "chosen_locations": ["location_id2"],
+            }
+        }
+
+
+class PartyPlanCreate(BaseModel):
+    # id, account_id, created, party_status auto-generated
+    account_id: str
+    api_maps_location: List[ApiMapsLocation]
+    start_time: datetime
+    end_time: Optional[datetime]
+    description: Optional[str]
+    image: Optional[HttpUrl]
+    keywords: Optional[List[str]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "account_id": "123456789",
+                "api_maps_location": [
+                    {
+                        "input": "New York, NY",
+                    }
+                ],
+                "start_time": "2022-02-23T14:30:00",
+                "end_time": "2022-02-23T17:30:00",
+                "description": "Updated Description here ....",
+                "image": "https://picsum.photos/201",
+                "keywords": ["party", "drinks", "dance"],
             }
         }
 
 
 class PartyPlanUpdate(BaseModel):
-    username: Optional[str]
-    notes: Optional[str]
-    date: Optional[date]
-    invitations: Optional[Dict[str, str]]
-    start_time: datetime
-    end_time: datetime
-    party_status: Optional[str]
-    keywords: Optional[List[str]]
-    general_location: Optional[str]
-    favorite_locations: Optional[Dict[str, str]]
-    chosen_locations: Optional[Dict[str, str]]
+    api_maps_location: Optional[List[ApiMapsLocation]]
+    start_time: Optional[datetime]
+    end_time: Optional[datetime]
     description: Optional[str]
     image: Optional[HttpUrl]
+    invitations: Optional[List[UUID]]
+    party_status: Optional[PartyStatus]
+    keywords: Optional[List[str]]
+    searched_locations: Optional[List[UUID]]
+    favorite_locations: Optional[List[UUID]]
+    chosen_locations: Optional[List[UUID]]
 
     class Config:
         schema_extra = {
             "example": {
-                "id": "generated",
-                "username": "Billy1234",
-                "notes": "Notes go here....",
-                "date": "2022-02-22",
-                "invitees": {
-                    "name": "id",
-                    "name": "id",
-                    "name": "id",
-                    "name": "id",
-                },
-                "start_time": "2022-02-22 14:30",
-                "end_time": "2022-02-22 17:30",
-                "party_status": "draft",
-                "keywords": ["fun", "bar", "burgers"],
-                "general_location": "Denver",
-                "favorite_locations": {
-                    "location name": "place_id",
-                    "location name": "place_id",
-                    "location name": "place_id",
-                    "location name": "place_id",
-                },
-                "description": "Description here ....",
-                "image": "https://picsum.photos/200",
+                "api_maps_location": [
+                    {
+                        "geo": {
+                            "type": "Point",
+                            "coordinates": [40.7128, -74.0060],
+                        },
+                        "input": "New York, NY",
+                    }
+                ],
+                "start_time": "2022-02-23T14:30:00",
+                "end_time": "2022-02-23T17:30:00",
+                "description": "Updated Description here ....",
+                "image": "https://picsum.photos/201",
+                "party_status": "share draft",
+                "invitations": ["<INSERT VALID INVITATION"],
+                "keywords": ["party", "drinks", "dance"],
+                "searched_locations": [
+                    "<placeid1 REPLACE ME>",
+                    "<placeid2 REPLACE ME>",
+                ],
+                "favorite_locations": [
+                    "<placeid1 REPLACE ME>",
+                    "<placeid2 REPLACE ME>",
+                ],
+                "chosen_locations": ["<placeid2 REPLACE ME>"],
             }
         }
--- api/routers/party_plans.py
+++ api/routers/party_plans.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Body, HTTPException, status, Response, Depends
 from fastapi.encoders import jsonable_encoder
 from typing import List
-from uuid import UUID
+from datetime import datetime
+from uuid import UUID, uuid4
+from bson.binary import Binary
+from datetime import datetime, timedelta
 from utils.authenticator import authenticator
-from models.party_plans import PartyPlan, PartyPlanUpdate
+from models.party_plans import PartyPlan, PartyPlanUpdate, PartyPlanCreate
 from clients.client import db
-
 
 
 router = APIRouter()
@@ -18,14 +20,39 @@ router = APIRouter()
     response_model=PartyPlan,
 )
 def create_party_plan(
-    plan: PartyPlan = Body(...),
+    party_plan: PartyPlanCreate = Body(...),
     # account: dict = Depends(authenticator.get_current_account_data),
 ):
-    plan = jsonable_encoder(plan)
-    new_plan = db.party_plan.insert_one(plan)
-    created_plan = db.party_plan.find_one({"_id": new_plan.inserted_id})
-    created_plan["_id"] = str(created_plan["_id"])
-    return created_plan
+    # generate id, timestamp, default status of draft
+    party_plan_data = jsonable_encoder(party_plan)
+    party_plan_data["id"] = str(uuid4())
+    party_plan_data["created"] = datetime.now()
+    party_plan_data["party_status"] = "draft"
+
+    # # expires geojson after 30 days
+    # expires = datetime.utcnow() + timedelta(minutes=1)
+    # # expires = datetime.utcnow() + timedelta(days=30)
+    # for location in party_plan_data.get("api_maps_location", []):
+    #     if location.get("geo") and location["geo"].get("coordinates"):
+    #         location["geo"]["expires"] = expires
+
+    # add to db
+    new_party_plan = db.party_plans.insert_one(party_plan_data)
+    if not new_party_plan.acknowledged:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to add party plan to database.",
+        )
+
+    # fetch the plan you just made
+    created_party_plan = db.party_plans.find_one({"id": party_plan_data["id"]})
+    if not created_party_plan:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Party plan with ID {party_plan_data['id']} not found after insertion.",
+        )
+
+    return created_party_plan
 
 
 @router.get(
@@ -36,8 +63,13 @@ def create_party_plan(
 def list_party_plans(
     # account: dict = Depends(authenticator.get_current_account_data),
 ):
-    parties = list(db.party_plan.find(limit=100))
-    return parties
+    party_plans = list(db.party_plans.find(limit=100))
+    for party in party_plans:
+        # fetch associated invitations
+        invitations = list(db.invitations.find({"party_plan_id": party["id"]}))
+        # return list of invitation ids
+        party["invitations"] = [inv["id"] for inv in invitations]
+    return party_plans
 
 
 @router.get(
@@ -49,41 +81,126 @@ def find_party_plan(
     id: str,
     # account: dict = Depends(authenticator.get_current_account_data),
 ):
-    if (party := db.party_plan.find_one({"_id": id})) is not None:
-        return party
+    party_plan = db.party_plans.find_one({"id": id})
+    if party_plan:
+        # fetch associated invitations
+        invitations = list(
+            db.invitations.find({"party_plan_id": party_plan["id"]})
+        )
+        # return list of invitation ids
+        party_plan["invitations"] = [inv["id"] for inv in invitations]
+
+        return party_plan
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Party with ID {id} not found",
+        detail=f"Party plan with ID {id} not found",
     )
 
 
 @router.put(
     "/{id}",
     response_description="Update a party plan",
-    response_model=PartyPlanUpdate,
+    response_model=PartyPlan,
 )
 def update_party_plan(
     id: UUID,
-    party: PartyPlanUpdate = Body(...),
+    party_plan: PartyPlanUpdate = Body(...),
     # account: dict = Depends(authenticator.get_current_account_data),
 ):
-    existing_party = db.party_plan.find_one({"_id": str(id)})
-
-    if not existing_party:
+    # get plan by id
+    existing_party_plan = db.party_plans.find_one({"id": str(id)})
+    if not existing_party_plan:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Party with ID {id} not found",
         )
 
-    if party.start_time:
-        party.date = party.date.isoformat()
+    party_plan_data = {
+        k: v for k, v in party_plan.dict().items() if v is not None
+    }
 
-    party_data = {k: v for k, v in party.dict().items() if v is not None}
+    # make sure searched_locations in payload are in locations database and update. this will append the searched_locations array with each update.
+    if "searched_locations" in party_plan_data:
+        for location_id in party_plan_data["searched_locations"]:
+            if location_id not in existing_searched_locations:
+                location = db.locations.find_one({"id": str(location_id)})
+                if not location:
+                    raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Location with ID {location_id} not found.",
+                    )
+                existing_searched_locations.append(location_id)
 
-    if party_data:
-        db.party_plan.update_one({"_id": str(id)}, {"$set": party_data})
+    # validate and update favorite_locations in payload. will overwrite existing list.
+    if "favorite_locations" in party_plan_data:
+        existing_searched_locations = existing_party_plan.get(
+            "searched_locations", []
+        )
+        # initialize
+        if party_plan_data["favorite_locations"] is None:
+            party_plan_data["favorite_locations"] = []
 
-    return db.party_plan.find_one({"_id": str(id)})
+        if not all(
+            fav_id in existing_searched_locations
+            for fav_id in party_plan_data["favorite_locations"]
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="All favorite locations must be part of searched locations.",
+            )
+        for fav_id in party_plan_data["favorite_locations"]:
+            db.locations.update_one(
+                {"id": str(fav_id)},
+                {"$set": {"favorite_status": True}},
+            )
+
+    # validate that chosen_locations are part of favorite_locations. will overwrite existing list.
+    if "chosen_locations" in party_plan_data:
+        existing_favorite_locations = existing_party_plan.get(
+            "favorite_locations", []
+        )
+        if not all(
+            chosen_id in existing_favorite_locations
+            for chosen_id in party_plan_data["chosen_locations"]
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="All chosen locations must be part of favorite locations.",
+            )
+        party_plan_data["chosen_locations"] = [
+            chosen_id for chosen_id in party_plan_data["chosen_locations"]
+        ]
+
+    # validate invitations in payload
+    if "invitations" in party_plan_data:
+        invitations_to_validate = party_plan_data["invitations"]
+        associated_invitations = list(
+            db.invitations.find({"party_plan_id": str(id)})
+        )
+        associated_invitation_ids = {
+            str(invitation["id"]) for invitation in associated_invitations
+        }
+        if not all(
+            str(invitation_id) in associated_invitation_ids
+            for invitation_id in invitations_to_validate
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Some provided invitation IDs are not associated with this party plan.",
+            )
+
+        # add validated invitation IDs to the party plan
+        party_plan_data["invitations"] = [
+            UUID(str(inv_id)) for inv_id in invitations_to_validate
+        ]
+
+    # timestamp
+    current_time = datetime.now()
+    party_plan_data["updated"] = current_time
+
+    db.party_plans.update_one({"id": str(id)}, {"$set": party_plan_data})
+
+    return db.party_plans.find_one({"id": str(id)})
 
 
 @router.delete("/{id}", response_description="Delete a party plan")
@@ -92,14 +209,12 @@ def delete_party_plan(
     response: Response,
     # account: dict = Depends(authenticator.get_current_account_data),
 ):
-    delete_result = db.party_plan.delete_one({"_id": id})
-
+    delete_result = db.party_plans.delete_one({"id": id})
     if delete_result.deleted_count == 1:
         return {
             "status": "success",
-            "message": f"Party plan with id {id}) successfully deleted.",
+            "message": f"Party plan with ID {id} successfully deleted.",
         }
-
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"No party plan with ID {id} found. Deletion incomplete.",
