33ee31c1720a20b622d20defed988b4e6710ad01
leahnp613 <leahnagarpowers@gmail.com>
2023-09-07 12:30:06 -0400

    added journal entry

--- ghi/src/utils/App.js
+++ ghi/src/utils/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import { DateProvider } from "./DateContext";
-import Main from "./Main";
+import Main from "../Main";
 import Dashboard from "./Dashboard";
 import UserDashboard from "./UserDashboard";
 import PartyPlanForm from "./PartyPlanForm";
-import PartyPlanDetail from "./PartyPlan/PartyPlanDetail";
+import PartyPlanDetail from "../PartyPlan/PartyPlanDetail";
 import InviteeDashboard from "./InviteeDashboard";
-import TestSpa from "./TestSpa";
-import { DashboardProvider } from "./utils/DashboardContext";
-import ProtectedRoute from "./ProtectedRoute";
+import TestSpa from "../Tests/TestSpa";
+import { DashboardProvider } from "./DashboardContext";
+import ProtectedRoute from "../ProtectedRoute";
 
 function App() {
   const domain = /https:\/\/[^/]+/;
